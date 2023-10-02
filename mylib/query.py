7532bd8de73519c13b43b18b168ab6bde8adf17b
Nick Strauch <61529274+NickStrauch13@users.noreply.github.com>
2023-10-02 01:12:15 +0000

    Added crud to readme

--- mylib/query.py
+++ mylib/query.py
@@ -7,6 +7,7 @@ import pandas as pd
 def get_df_and_print_head(query, conn, head_size=10):
     """Return a dataframe and print the head"""
     df = pd.read_sql_query(query, conn)
+    # drop frist row
     df.reset_index(drop=True, inplace=True)
     print(df.head(head_size))
     return df
@@ -15,8 +16,17 @@ def get_df_and_print_head(query, conn, head_size=10):
 def query_db(query="SELECT * FROM seattle_weather LIMIT 6"):
     """Query the database given an input query"""
     conn = sqlite3.connect("Weather.db")
-    df = get_df_and_print_head(query, conn, head_size=10)
+    if query.lower().startswith("select"):
+        df = get_df_and_print_head(query, conn, head_size=10)
+        conn.close()
+        return df
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
+    print("Query executed successfully.")
+    query_results = cursor.fetchall()
     conn.close()
-    return df
+    return query_results
+
 
 
